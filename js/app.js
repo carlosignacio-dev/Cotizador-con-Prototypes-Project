5fb532ca6d40336c084c8e7b7b27c96dfcd01a95
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-20 10:57:39 -0400

    added validacion

--- js/app.js
+++ js/app.js
@@ -29,3 +29,30 @@ const ui = new UI();
 document.addEventListener("DOMContentLoaded", () => {
     ui.llenarOpciones();//Llena el select con los años
 })
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener("submit", cotizarSeguro);
+}
+
+//Validacion
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    //Leer marca seleccionada
+    const marca = document.querySelector("#marca").value;
+
+    //Leer año seleccionado
+    const year = document.querySelector("#year").value;
+
+    //Leer tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    
+    if(marca === "" || year === "" || tipo === "") {
+        console.log("No seleccionado");
+    } else {
+        console.log("Seleccionado");
+    }
+}
